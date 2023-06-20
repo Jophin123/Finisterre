452597992b9617c66da872ddcd1f890174840d5b
Jophin Francis <jophinfrancis1997@gmail.com>
2023-06-21 00:29:04 +0100

    f

--- Script.js
+++ Script.js
@@ -3,4 +3,21 @@
 //       setting-name: data-slick
 //     });
 //   });
-$('.slick-carousel').slick();
+// $('.slick-carousel').slick();
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
